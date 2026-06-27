2f7cf7ad12e6a3a79601bffd7b0c490e8dd05892
danechristensen21-prog <dane.christensen21@gmail.com>
2026-06-27 11:12:24 -0400

    Create Window.jsx

---
+++ src/objects/Decor/Window.jsx
@@ -0,0 +1,11 @@
+export default function Window() {
+  return (
+    <div className="window-object">
+      <div className="window-sky">
+        <div className="sun" />
+        <div className="cloud cloud-one" />
+        <div className="cloud cloud-two" />
+      </div>
+    </div>
+  );
+}
