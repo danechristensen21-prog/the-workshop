ba9e69ae0605006d7c17e44f318d1dfa080e1c2d
danechristensen21-prog <dane.christensen21@gmail.com>
2026-06-27 11:16:34 -0400

    Create CoffeeStation.jsx

---
+++ src/objects/Decor/CoffeeStation.jsx
@@ -0,0 +1,10 @@
+export default function CoffeeStation() {
+  return (
+    <div className="coffee-object">
+      <div className="steam steam-one" />
+      <div className="steam steam-two" />
+      <div className="coffee-cup">☕</div>
+      <small>Morning Fuel</small>
+    </div>
+  );
+}
