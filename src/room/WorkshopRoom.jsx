dae46b06bd940d8eaa173e65ff8ec28d2b8355ef
danechristensen21-prog <dane.christensen21@gmail.com>
2026-06-27 11:09:32 -0400

    Create WorkshopRoom.jsx

---
+++ src/room/WorkshopRoom.jsx
@@ -0,0 +1,47 @@
+import { roomObjects } from "../data/roomObjects";
+import RoomObject from "./RoomObject";
+
+import Whiteboard from "../objects/Whiteboard/Whiteboard";
+import Desk from "../objects/Desks/Desk";
+import Window from "../objects/Decor/Window";
+import CoffeeStation from "../objects/Decor/CoffeeStation";
+import BuckyBed from "../objects/Decor/BuckyBed";
+import LavaLamp from "../objects/Decor/LavaLamp";
+import TrophyShelf from "../objects/Decor/TrophyShelf";
+import FileCabinet from "../objects/Decor/FileCabinet";
+
+const objectComponents = {
+  Whiteboard,
+  Desk,
+  Window,
+  CoffeeStation,
+  BuckyBed,
+  LavaLamp,
+  TrophyShelf,
+  FileCabinet,
+};
+
+export default function WorkshopRoom() {
+  return (
+    <main className="workshop-room">
+      <div className="room-light room-light-one" />
+      <div className="room-light room-light-two" />
+
+      <section className="room-stage">
+        <div className="neon-sign">IDEAS BECOME REAL HERE</div>
+        <div className="back-wall" />
+        <div className="floor" />
+
+        {roomObjects.map((object) => {
+          const Component = objectComponents[object.component];
+
+          return (
+            <RoomObject key={object.id} object={object}>
+              <Component object={object} />
+            </RoomObject>
+          );
+        })}
+      </section>
+    </main>
+  );
+}
