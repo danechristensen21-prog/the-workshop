f9a494f71ca00e5bcf0d50dbad7288a50f4bb6a6
danechristensen21-prog <dane.christensen21@gmail.com>
2026-06-27 11:18:07 -0400

    Create FileCabinet.jsx

---
+++ src/objects/Decor/FileCabinet.jsx
@@ -0,0 +1,10 @@
+export default function FileCabinet() {
+  return (
+    <div className="file-cabinet-object">
+      <strong>FILES</strong>
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+}
