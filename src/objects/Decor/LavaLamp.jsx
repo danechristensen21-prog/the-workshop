79562d1442d93c8cb4b614fd3786fc6aac6df6ac
danechristensen21-prog <dane.christensen21@gmail.com>
2026-06-27 11:17:19 -0400

    Create LavaLamp.jsx

---
+++ src/objects/Decor/LavaLamp.jsx
@@ -0,0 +1,12 @@
+export default function LavaLamp() {
+  return (
+    <div className="lava-object">
+      <div className="lava-cap" />
+      <div className="lava-glass">
+        <div className="lava-blob blob-one" />
+        <div className="lava-blob blob-two" />
+      </div>
+      <div className="lava-base" />
+    </div>
+  );
+}
